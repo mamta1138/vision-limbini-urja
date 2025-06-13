64fc1a80395aeecd043071033cefd2b55274dc91
mamta1138 <ghimire.mamta98@gmail.com>
2025-06-13 12:22:04 +0545

    Refactor report update process: include file URL in report data and remove verify report functionality from routes.

--- components/report/routers/report_routes.js
+++ components/report/routers/report_routes.js
@@ -10,7 +10,6 @@ const listAllReports = require("../controllers/list_all_reports");
 const getSingleReport = require("../controllers/get_single_report");
 const updateReport = require("../controllers/update_report");
 const deleteReport = require("../controllers/delete_report");
-const verifyReport = require("../controllers/verify_report");
 
 router.post("/", verifyToken, checkRole("admin", "editor"), upload.single("file"), createReport);
 
@@ -18,9 +17,7 @@ router.get("/", listAllReports);
 
 router.get("/:id", getSingleReport);
 
-router.put("/:id", verifyToken, checkRole("admin", "editor"), updateReport);
-
-router.put("/verify/:id", verifyToken, checkRole("admin"), verifyReport);
+router.put("/:id", verifyToken, checkRole("admin", "editor"), upload.single("file"),  updateReport);
 
 router.delete("/:id", verifyToken, checkRole("admin"), deleteReport);
 
